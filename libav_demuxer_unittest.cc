5876d6d990ed48aaf875570c4e6b824b1cc2b128
Simon Horlick <simonhorlick@gmail.com>
2012-05-26 12:00:07 +0100

    Add demuxer initialisation test
    Test for no streams in an input file.

--- libav_demuxer_unittest.cc
+++ libav_demuxer_unittest.cc
@@ -3,17 +3,30 @@
 class LibavDemuxerTest : public testing::Test {
 public:
     LibavDemuxerTest() {
-
+        demuxer_ = new LibavDemuxer();
+        source_ = 0;
     }
 
     ~LibavDemuxerTest() {
-
+        delete demuxer_;
+        if(source_) delete source_;
     }
 
+    void DoInitialise(const char* filename) {
+        ASSERT_TRUE(source_ == 0);
+        source_ = new FileSource(filename);
+        bool init = demuxer_->Initialise(source_);
+        ASSERT_TRUE(init);
+    }
+
+    LibavDemuxer* demuxer_;
+    DataSource* source_;
 };
 
 TEST_F(LibavDemuxerTest, InitialiseNoStreams) {
-    FAIL();
+    DoInitialise("test/nostreams.ts");
+    std::map<int, Stream*>& streams = demuxer_->GetStreams();
+    EXPECT_TRUE(streams.empty());
 }
 
 TEST_F(LibavDemuxerTest, InitialiseNoRecognisedStreams) {
