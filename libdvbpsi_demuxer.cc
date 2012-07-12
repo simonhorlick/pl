7f69c5db272141226fb8a9b76abcced92fd0a179
Simon Horlick <simonhorlick@gmail.com>
2012-07-12 21:28:24 +0100

    Add empty functions

---
+++ libdvbpsi_demuxer.cc
@@ -0,0 +1,19 @@
+#include "libdvbpsi_demuxer.h"
+#include "data_source.h"
+
+bool LibdvbpsiDemuxer::Initialise(DataSource* source) {
+
+}
+
+void LibdvbpsiDemuxer::PostDemuxTask() {
+
+}
+
+void LibdvbpsiDemuxer::Demux() {
+
+}
+
+void LibdvbpsiDemuxer::RegisterProgrammeCallback() {
+
+}
+
