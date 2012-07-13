51f3062a322b14628a55c815c10b9c41cfebbd8c
Simon Horlick <simonhorlick@gmail.com>
2012-07-13 20:03:43 +0100

    Handle demuxing of PMT tables.

--- libdvbpsi_demuxer.h
+++ libdvbpsi_demuxer.h
@@ -2,6 +2,7 @@
 #define LIBDVBPSI_DEMUXER_H
 
 #include <stdint.h>
+#include <map>
 
 class DataSource;
 typedef struct dvbpsi_s dvbpsi_t;
@@ -13,17 +14,25 @@ public:
 
     bool Initialise(DataSource* source);
 
+    void Cleanup();
+
     void PostDemuxTask();
 
     void Demux();
 
     void RegisterProgrammeCallback();
 
+    // FIXME: this sucks
+    std::map<uint16_t, dvbpsi_t*>& GetPMTMap() {
+        return pmt_;
+    }
+
 private:
     bool ReadPacket(uint8_t* buffer);
 
     DataSource* source_;
     dvbpsi_t* context_;
+    std::map<uint16_t, dvbpsi_t*> pmt_;
 };
 
 #endif // LIBDVBPSI_DEMUXER_H
