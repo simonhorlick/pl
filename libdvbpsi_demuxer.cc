e5afb765f96ead9841a09df3f4e727369082970b
Simon Horlick <simonhorlick@gmail.com>
2012-07-13 20:03:43 +0100

    Handle demuxing of PMT tables.

--- libdvbpsi_demuxer.cc
+++ libdvbpsi_demuxer.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <map>
 
 #include "libdvbpsi_demuxer.h"
 #include "data_source.h"
@@ -7,6 +8,8 @@ extern "C" {
     #include <dvbpsi/dvbpsi.h>
     #include <dvbpsi/psi.h>
     #include <dvbpsi/pat.h>
+    #include <dvbpsi/descriptor.h>
+    #include <dvbpsi/pmt.h>
 }
 
 static void debug_print(dvbpsi_t *handle, const dvbpsi_msg_level_t level, const char* msg) {
@@ -19,7 +22,54 @@ static void debug_print(dvbpsi_t *handle, const dvbpsi_msg_level_t level, const 
     std::cout << msg << '\n';
 }
 
-static void PATCallback(void* zero, dvbpsi_pat_t* table) {
+static const char* GetStreamTypeString(uint8_t type) {
+    switch (type) {
+        case 0x00: return "Reserved";
+        case 0x01: return "ISO/IEC 11172 Video";
+        case 0x02: return "ISO/IEC 13818-2 Video";
+        case 0x03: return "ISO/IEC 11172 Audio";
+        case 0x04: return "ISO/IEC 13818-3 Audio";
+        case 0x05: return "ISO/IEC 13818-1 Private Section";
+        case 0x06: return "ISO/IEC 13818-1 Private PES data packets";
+        case 0x07: return "ISO/IEC 13522 MHEG";
+        case 0x08: return "ISO/IEC 13818-1 Annex A DSM CC";
+        case 0x09: return "H222.1";
+        case 0x0A: return "ISO/IEC 13818-6 type A";
+        case 0x0B: return "ISO/IEC 13818-6 type B";
+        case 0x0C: return "ISO/IEC 13818-6 type C";
+        case 0x0D: return "ISO/IEC 13818-6 type D";
+        case 0x0E: return "ISO/IEC 13818-1 auxillary";
+        default:
+            if (type < 0x80)
+                return "ISO/IEC 13818-1 reserved";
+            else
+                return "User Private";
+    }
+}
+
+static void PMTCallback(void* zero, dvbpsi_pmt_t* table) {
+    std::cout << "PMT\n";
+    std::cout << "  program number is " << table->i_program_number << '\n';
+    std::cout << "  version number is " << table->i_version << '\n';
+    std::cout << "  pcr pid is " << table->i_pcr_pid << '\n';
+
+    // descriptors: p_pmt->p_first_descriptor
+
+    dvbpsi_pmt_es_t* stream = table->p_first_es;
+    std::cout << "  elementary streams:\n";
+    while(stream) {
+        std::cout << "    pid is " << stream->i_pid << " type " <<
+            GetStreamTypeString(stream->i_type) << '\n';
+        stream = stream->p_next;
+    }
+
+    dvbpsi_DeletePMT(table);
+}
+
+// Create a PMT handler for each entry in the PAT
+static void PATCallback(void* opaque, dvbpsi_pat_t* table) {
+    LibdvbpsiDemuxer* demuxer = reinterpret_cast<LibdvbpsiDemuxer*>(opaque);
+    std::map<uint16_t, dvbpsi_t*>& pmt_ = demuxer->GetPMTMap();
     dvbpsi_pat_program_t* program = table->p_first_program;
 
     std::cout << "PAT\n";
@@ -28,6 +78,22 @@ static void PATCallback(void* zero, dvbpsi_pat_t* table) {
     std::cout << "  active " << table->b_current_next << '\n';
     while(program) {
         std::cout << "  program " << program->i_number << " has pid " << program->i_pid << '\n';
+
+        std::map<uint16_t, dvbpsi_t*>::iterator it = pmt_.find(program->i_pid);
+        if(it != pmt_.end()) {
+            std::clog << "Replacing existing PMT listener for pid " << program->i_pid << '\n';
+            dvbpsi_DetachPMT(it->second);
+            dvbpsi_DeleteHandle(it->second);
+            pmt_.erase(it);
+        }
+
+        std::clog << "Attaching new PMT listener";
+        pmt_[program->i_pid] = dvbpsi_NewHandle(&debug_print, DVBPSI_MSG_DEBUG);
+        if(dvbpsi_AttachPMT(pmt_[program->i_pid], program->i_number, PMTCallback, 0)) {
+            std::cerr << "Failed to start PMT callback\n";
+            return false;
+        }
+
         program = program->p_next;
     }
 
@@ -43,12 +109,20 @@ bool LibdvbpsiDemuxer::Initialise(DataSource* source) {
         return false;
     }
 
+    // A PAT contains n PMT references. 
     if(!dvbpsi_AttachPAT(context_, PATCallback, NULL)) {
         std::cerr << "Failed to start PAT callback\n";
         return false;
     }
 
     return true;
+}
+
+void LibdvbpsiDemuxer::Cleanup() {
+    if(context_) {
+        dvbpsi_DetachPAT(context_);
+        dvbpsi_DeleteHandle(context_);
+    }
 }
 
 void LibdvbpsiDemuxer::PostDemuxTask() {
@@ -78,8 +152,13 @@ void LibdvbpsiDemuxer::Demux() {
     int ok = ReadPacket(buffer);
     while(ok) {
         uint16_t pid = ((uint16_t)(buffer[1] & 0x1f) << 8) + buffer[2];
-        if(pid == 0x0)
+        if(pid == 0x0) {
+            std::clog << "Got PAT packet (pid 0)\n";
             dvbpsi_PushPacket(context_, buffer);
+        } else if(pmt_.find(pid) != pmt_.end()) {
+            std::clog << "Got PMT packet (pid "<<pid<<")\n";
+            dvbpsi_PushPacket(pmt_[pid], buffer);
+        }
         ok = ReadPacket(buffer);
     }
 }
