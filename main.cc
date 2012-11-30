957e926a6f7f894a77ec5614e8eb33814addd03c
Simon Horlick <simonhorlick@gmail.com>
2012-11-30 21:23:16 +0000

    Add stream to main

--- main.cc
+++ main.cc
@@ -6,6 +6,18 @@
 #include "elementary_stream.h"
 #include "file_renderer.h"
 
+ElementaryStream* CreateStream(MulticastSource& source) {
+    // Run this in the network thread
+    ElementaryStream* stream = new ElementaryStream(ElementaryStream::kVideoStream);
+    const int readbufsize = 4096;
+    char buf[readbufsize];
+    while(true) {
+        int bytes = source.Read(buf, readbufsize);
+        stream->AddPacket((uint8_t*)buf, (uint8_t*)buf+bytes);
+    }
+    return stream;
+}
+
 int main(int argc, char** argv) {
     MulticastSource source("239.255.0.54", 5004);
  
